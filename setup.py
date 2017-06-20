e7e092dbd96dd91794e55fd482fe4cf2c859372b
Babajide Ogunjobi <broadwayoni@JideO.local>
2017-06-20 00:33:16 -0400

    updated tag version

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'qtx',
   packages = ['qtx'],
-  version = '0.1',
+  version = '0.2',
   description = 'API client for Quantextive AEX Data Platform',
   author = 'Quantextive',
   author_email = 'api@quantextive.com',
   url = 'https://github.com/quantextive/qtx', #github repo
-  download_url = 'https://github.com/quantextive/qtx/archive/0.1.tar.gz',
+  download_url = 'https://github.com/quantextive/qtx/archive/0.2.tar.gz',
   keywords = ['africa', 'finance', 'quantextive'],
   classifiers = [],
 )
